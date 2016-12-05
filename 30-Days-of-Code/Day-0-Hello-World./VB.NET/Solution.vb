c6f96afd564fefef5252d505b8ee2d41894402b1
qxzsilver1 <hk442@cornell.edu>
2016-12-05 18:45:02 -0500

    Solution in VB .NET framework

---
+++ 30-Days-of-Code/Day-0-Hello-World./VB.NET/Solution.vb
@@ -0,0 +1,19 @@
+Imports System
+
+Module Solution
+    
+    Public Shared Sub Main()
+        ' Create a String variable:
+        Dim greeting As String 
+
+        ' Read value from stdin and save it to variable:
+        greeting = Console.ReadLine() 
+
+        ' Print "Hello, World." to stdout:
+        Console.WriteLine("Hello, World.")
+        
+        ' Write your code here; print the contents of the 'greeting' variable to stdout.
+        Console.WriteLine(greeting)
+
+    End Sub
+End Module
