69b185d2e69ecd83d689a81c55b501f08fe619ed
qxzsilver1 <hk442@cornell.edu>
2016-12-07 16:48:39 -0500

    Create Solution.c

---
+++ 30-Days-of-Code/Day-1-Data-Types/C/Solution.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+    
+    // Declare second integer, double, and String variables.
+    int j;
+    double e;
+    char t[105];
+    
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d", &j);
+    scanf("\n%lf", &e);
+    scanf("\n%[^\n]", t);
+    
+    // Print the sum of both integer variables on a new line.
+    printf("%d\n", i+j);
+    
+    // Print the sum of the double variables on a new line.
+    printf("%.1lf\n", d+e);
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    printf("%s", s);
+    printf("%s\n", t);
+    
+
+
+    return 0;
+}
