991abc083b1d520ccd566d4d8093ec368906d04d
qxzsilver1 <hk442@cornell.edu>
2016-12-25 02:56:36 -0500

    Create Solution.cc

---
+++ 30-Days-of-Code/Day-1-Data-Types/CPP/Solution.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    // Declare second integer, double, and String variables.
+    int j;
+    double e;
+    string t;
+    
+    // Read and save an integer, double, and String to your variables.
+    std::cin >> j;
+    std::cin >> e;
+    cin.ignore();
+    std::getline(cin, t);
+    
+    // Print the sum of both integer variables on a new line.
+    std::cout << i + j << endl;
+    
+    // Print the sum of the double variables on a new line.
+    std::cout << fixed << std::setprecision(1) << d + e << endl;
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    std::cout << s + t << endl;
+
+
+    return 0;
+}
