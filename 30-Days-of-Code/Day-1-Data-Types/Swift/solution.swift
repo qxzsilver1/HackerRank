ae578eb3ccddeaa598ca0f516b7576026b3f4d6d
qxzsilver1 <hk442@cornell.edu>
2016-12-25 13:17:25 -0500

    Create solution.swift

---
+++ 30-Days-of-Code/Day-1-Data-Types/Swift/solution.swift
@@ -0,0 +1,23 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var j:Int
+var e:Double
+var t:String
+
+// Read and save an integer, double, and String to your variables.
+j = Int(readLine()!)!
+e = Double(readLine()!)!
+t = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i+j)
+
+// Print the sum of the double variables on a new line.
+print(d+e)
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s+t)
