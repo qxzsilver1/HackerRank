5bac2e8819ee82f0d4f3a5cba748f6ba0c774973
qxzsilver1 <hk442@cornell.edu>
2016-12-25 12:47:37 -0500

    Create solution.py

---
+++ 30-Days-of-Code/Day-1-Data-Types/Python2/solution.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+j = 0
+e = 0.0
+t = None
+
+# Read and save an integer, double, and String to your variables.
+j = eval(raw_input())
+e = eval(raw_input())
+t = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i+j
+
+# Print the sum of the double variables on a new line.
+print d+e
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s+t
