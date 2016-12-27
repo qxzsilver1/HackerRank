d3d0e9ba4bd7e89cc42f19680760742303308c5f
qxzsilver1 <hk442@cornell.edu>
2016-12-27 01:36:35 -0500

    Create solution.cbl

---
+++ COBOL/THEORY -1/solution.cbl
@@ -0,0 +1,13 @@
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. THEORY.
+       
+       ENVIRONMENT DIVISION.
+      
+       DATA DIVISION.
+       
+       PROCEDURE DIVISION.
+            
+            STOP RUN.
+
+       END PROGRAM THEORY.
