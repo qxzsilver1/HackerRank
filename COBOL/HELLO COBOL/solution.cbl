83279996deca71a06b9501cca03ed55750389034
qxzsilver1 <hk442@cornell.edu>
2016-12-27 01:34:54 -0500

    Create solution.cbl

---
+++ COBOL/HELLO COBOL/solution.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO_COBOL.
+      
+       ENVIRONMENT DIVISION.
+      
+       DATA DIVISION.
+      
+       PROCEDURE DIVISION.
+   
+         DISPLAY "hello cobol". 
+      
+      STOP RUN.
