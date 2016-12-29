daed3500d32832d3fee2baeb8b78ae1f0f946d55
qxzsilver1 <hk442@cornell.edu>
2016-12-29 01:15:21 -0500

    Create solution.c

---
+++ 30-Days-of-Code/Day-3-Intro-to-Conditional-Statements/C/solution.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int N; 
+    scanf("%d",&N);
+    
+    if (N % 2 == 1)
+        printf("Weird\n");
+    else {
+        if (N >= 6 && N <= 20)
+            printf("Weird\n");
+        else
+            printf("Not Weird\n");
+    }
+    
+    return 0;
+}
