cf7fc5aa1cd9e7a753d5e7f6eadba6f1bf76449c
qxzsilver1 <hk442@cornell.edu>
2017-01-02 14:35:13 -0500

    Update solution.cc

--- 30-Days-of-Code/Day-4-Class-vs.-Instance/CPP/solution.cc
+++ 30-Days-of-Code/Day-4-Class-vs.-Instance/CPP/solution.cc
@@ -10,18 +10,25 @@ class Person{
     };
 
     Person::Person(int initialAge){
-        // Add some more code to run some checks on initialAge
-
+        age = initialAge;
+        
+        if (initialAge < 0) {
+            age = 0;
+            cout << "Age is not valid, setting age to 0." << endl;
+        }            
     }
 
     void Person::amIOld(){
-        // Do some computations in here and print out the correct statement to the console 
-
+        if (age < 13)
+            cout << "You are young." << endl;
+        else if (age >= 13 && age < 18)
+            cout << "You are a teenager." << endl;
+        else
+            cout << "You are old." << endl;
     }
 
     void Person::yearPasses(){
-        // Increment the age of the person in here
-
+        age++;
     }
     
     int main(){
