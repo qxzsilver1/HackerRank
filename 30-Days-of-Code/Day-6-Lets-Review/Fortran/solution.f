ad864fbd9593d82e18782065debac98f934d6a64
qxzsilver1 <hk442@cornell.edu>
2017-01-16 15:08:52 -0500

    Update solution.f

--- 30-Days-of-Code/Day-6-Lets-Review/Fortran/solution.f
+++ 30-Days-of-Code/Day-6-Lets-Review/Fortran/solution.f
@@ -1,14 +1,32 @@
 INTEGER :: num
 CHARACTER(10000) :: line
+CHARACTER(:),ALLOCATABLE :: firstHalf, secondHalf
+CHARACTER(10001) :: output
 
 READ *, num
 
 iloop: do i = 1, num
     READ (*,'(a)') line
-    WRITE (*, '(a)') line
+    !WRITE (*, '(a)') TRIM(line)
     
-    !jloop: do j = 1,
-    !end do jloop
+    jloop: do j = 1, LEN_TRIM(line)
+        IF (MOD(j, 2)==1) THEN
+            firstHalf = firstHalf // line(j:j)
+            !PRINT *, firstHalf
+        ELSE
+            secondHalf = TRIM(secondHalf) // line(j:j)
+            !PRINT *, secondHalf
+        ENDIF
+    end do jloop
+    
+    output = TRIM(firstHalf) // " " // secondHalf
+    
+    WRITE(*,'(a)') TRIM(output)
+    
+    line = ""
+    firstHalf = ""
+    secondHalf = ""
+    
 end do iloop
 
-END
+end
