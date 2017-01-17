7b01caee6443fafef5e9ed3544cb509bb2126d1d
qxzsilver1 <hk442@cornell.edu>
2017-01-17 18:59:30 -0500

    Create solution.rb

---
+++ 30-Days-of-Code/Day-6-Lets-Review/Ruby/solution.rb
@@ -0,0 +1,22 @@
+n = gets.strip.to_i
+words = Array.new(n)
+
+(0..n-1).each do |i|
+    words[i] = gets.strip
+end
+
+
+def process(word)
+    letters = word.split("")
+    firstHalf = []
+    secondHalf = []
+    letters.each_with_index do |l, i|
+        i.even? ? firstHalf << l : secondHalf << l
+    end
+    
+    "#{firstHalf.join} #{secondHalf.join}"
+end
+
+words.each do |word|
+    puts process(word)
+end
