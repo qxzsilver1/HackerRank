512863254e54c3eb4d1c33e59725fed4b312a95d
qxzsilver1 <hk442@cornell.edu>
2017-01-17 23:51:13 -0500

    Create solution.py

---
+++ Regex/Introduction/Matching-Anything-But-Newline/Python2/solution.py
@@ -0,0 +1,10 @@
+regex_pattern = r"^...\....\....\....$"	# Do not delete 'r'.
+
+import re
+import sys
+
+test_string = raw_input()
+
+match = re.match(regex_pattern, test_string) is not None
+
+print str(match).lower()
