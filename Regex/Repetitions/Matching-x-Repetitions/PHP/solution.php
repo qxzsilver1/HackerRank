3569db1488afa04ae61177fbdec4e670ef06f7da
qxzsilver1 <hk442@cornell.edu>
2017-01-19 19:30:50 -0500

    Create solution.php

---
+++ Regex/Repetitions/Matching-x-Repetitions/PHP/solution.php
@@ -0,0 +1,14 @@
+<?php
+
+$Regex_Pattern = "/^[A-Za-z02468]{40}[\s13579]{5}$/"; //Do not delete '/'. Replace __________ with your regex.
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+if(preg_match($Regex_Pattern, $Test_String, $output_array)){
+    print ("true");
+} else {
+    print ("false");
+}
+
+fclose($handle);
+?>
