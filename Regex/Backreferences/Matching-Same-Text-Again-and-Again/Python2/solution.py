bab60017f4fb9032bd966a1b2a50c658dfbd4f3a
qxzsilver1 <hk442@cornell.edu>
2017-01-19 22:18:18 -0500

    Create solution.py

---
+++ Regex/Backreferences/Matching-Same-Text-Again-and-Again/Python2/solution.py
@@ -0,0 +1,5 @@
+Regex_Pattern = r'^([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([A-Za-z])([aeiouAEIOU])(\S)(\1)(\2)(\3)(\4)(\5)(\6)(\7)(\8)(\9)(\10)$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, raw_input()))).lower())
