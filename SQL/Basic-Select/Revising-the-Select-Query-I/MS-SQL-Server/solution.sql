b5e0beadfb1506724f168ecc2695cdabca427418
qxzsilver1 <hk442@cornell.edu>
2017-01-20 15:13:46 -0500

    Update solution.sql

--- SQL/Basic-Select/Revising-the-Select-Query-I/MS-SQL-Server/solution.sql
+++ SQL/Basic-Select/Revising-the-Select-Query-I/MS-SQL-Server/solution.sql
@@ -1,3 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
 SELECT *
 FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
