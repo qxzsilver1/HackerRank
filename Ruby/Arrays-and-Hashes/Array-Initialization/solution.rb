288cbfd63f0a66a1e3cc462043084483abedb2b8
qxzsilver1 <hk442@cornell.edu>
2017-01-21 15:42:40 -0500

    Create solution.rb

---
+++ Ruby/Arrays-and-Hashes/Array-Initialization/solution.rb
@@ -0,0 +1,10 @@
+# Initialize 3 variables here as explained in the problem statement
+
+array = Array.new
+# array = []
+
+array_1 = Array.new(1)
+# array_1 = [nil]
+
+array_2 = Array.new(2, 10)
+# array_2 = [10, 10]
