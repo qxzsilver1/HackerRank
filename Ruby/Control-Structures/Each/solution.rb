bcc5cedddc05ab685f455ebaa4482667aa2c0460
qxzsilver1 <hk442@cornell.edu>
2017-01-21 15:11:20 -0500

    Create solution.rb

---
+++ Ruby/Control-Structures/Each/solution.rb
@@ -0,0 +1,11 @@
+def scoring(array)
+    # iterate through each of the element in array using *each* and call update_score on it
+    array.each do |user|
+        user.update_score
+    end
+end
+
+## another way:
+# def scoring(array)
+#    array.each(&:update_score)
+# end
