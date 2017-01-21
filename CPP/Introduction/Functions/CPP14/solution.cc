d87267ac9968f6730503e95331ff31530cf31206
qxzsilver1 <hk442@cornell.edu>
2017-01-21 13:20:16 -0500

    Create solution.cc

---
+++ CPP/Introduction/Functions/CPP14/solution.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d) {
+    int val = a;
+    
+    if (val <= b)
+        val = b;
+    
+    if (val <= c)
+        val = c;
+    
+    if (val <= d)
+        val = d;
+        
+    return val;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
