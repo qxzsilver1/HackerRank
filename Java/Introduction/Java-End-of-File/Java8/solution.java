7b8998120f441933f4b7b5cef22eb47456de32d0
qxzsilver1 <hk442@cornell.edu>
2017-01-22 15:16:16 -0500

    Create solution.java

---
+++ Java/Introduction/Java-End-of-File/Java8/solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        
+        while(sc.hasNext()){
+            i++;
+            System.out.println(i + " " + sc.nextLine());
+        }
+        
+        sc.close();
+    }
+}
