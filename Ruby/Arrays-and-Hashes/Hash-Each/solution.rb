ee6b58ab5ab61cc3da96af7f427339f802303a06
qxzsilver1 <hk442@cornell.edu>
2017-01-21 21:15:10 -0500

    Create solution.rb

---
+++ Ruby/Arrays-and-Hashes/Hash-Each/solution.rb
@@ -0,0 +1,11 @@
+def iter_hash(hash)
+    hash.each do |arr|
+       puts arr[0]
+       puts arr[1]
+    end
+    #or:
+    # hash.each do |key, value|
+    #   puts key
+    #   puts value
+    # end
+end
