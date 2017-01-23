71af6ddb556972d9edf131d37e0aad7ec851e7d1
qxzsilver1 <hk442@cornell.edu>
2017-01-23 00:41:22 -0500

    Create solution.cc

---
+++ CPP/Strings/Strings/CPP/solution.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   // Complete the program
+    string a;
+    string b;
+    
+    cin >> a;
+    cin >> b;
+    
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    cout << b.front() + a.substr(1, a.size()-1) << " " << a.front() + b.substr(1, b.size()-1) << endl; 
+  
+    return 0;
+}
