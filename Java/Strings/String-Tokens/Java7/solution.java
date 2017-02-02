4c8c0067b7fa76016f5f8b735d91525116341f15
qxzsilver1 <hk442@cornell.edu>
2017-02-02 12:46:35 -0500

    Create solution.java

---
+++ Java/Strings/String-Tokens/Java7/solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+
+        s = s.trim();
+        if (s.length()>400000 || s == null){
+            return ;
+        } 
+        else if (s.length() == 0){
+            System.out.println(0);
+            return ;
+        }
+        else{   
+            String[] words = s.split("[!,?.*_'@\\ ]+");
+            int count = words.length;
+            System.out.println(count);
+            for(String word : words){
+                System.out.println(word);
+            }
+        }
+        scan.close();
+    }
+}
