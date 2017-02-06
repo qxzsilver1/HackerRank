98bcbc7f0db17132bbe07ed016df0f3570d1e2ee
qxzsilver1 <hk442@cornell.edu>
2017-02-06 17:44:50 -0500

    Update solution.py

--- Python/Strings/Text-Wrap/Pypy3/solution.py
+++ Python/Strings/Text-Wrap/Pypy3/solution.py
@@ -1,6 +1,6 @@
 import textwrap
 def wrap(string, max_width):
-    return  textwrap.fill(string, max_width)
+    return textwrap.fill(string, max_width)
     
 if __name__ == '__main__':
     string, max_width = input(), int(input())
