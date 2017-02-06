ae2446e2b78d7547a7e0ee47bce03edfaef303c5
qxzsilver1 <hk442@cornell.edu>
2017-02-06 03:00:19 -0500

    Create solution.sh

---
+++ Linux-Shell/Bash/More-on-Conditionals/solution.sh
@@ -0,0 +1,11 @@
+read X
+read Y
+read Z
+
+if [ $X -eq $Y ] && [ $Y -eq $Z ]; then
+    echo "EQUILATERAL"
+elif [ $X -eq $Y ] || [ $Y -eq $Z ]; then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
