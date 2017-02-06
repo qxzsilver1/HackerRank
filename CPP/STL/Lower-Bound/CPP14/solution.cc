fabaeb0c1872a161421899cc5560c84b73df94c8
qxzsilver1 <hk442@cornell.edu>
2017-02-06 16:41:56 -0500

    Create solution.cc

---
+++ CPP/STL/Lower-Bound/CPP14/solution.cc
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N, x;
+    cin >> N;
+    vector<int> v;
+    
+    for (int i = 0; i < N; i++){
+        cin >> x;
+        v.push_back(x);
+    }
+    
+    int q, y;
+    cin >> q;
+    for (int i = 0; i < q; i++){
+        cin >> y;
+        vector<int>::iterator low = lower_bound(v.begin(), v.end(), y);
+        if (v[low - v.begin()] == y)
+            cout << "Yes " << (low - v.begin()+1) << endl;
+        else
+            cout << "No " << (low - v.begin()+1) << endl;
+    }
+    
+    return 0;
+}
