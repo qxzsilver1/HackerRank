0a263b326efb0af3b501b55e757c4af927ec4896
qxzsilver1 <hk442@cornell.edu>
2017-02-06 18:28:37 -0500

    Create solution.py

---
+++ Python/Strings/The-Minion-Game/Python3/solution.py
@@ -0,0 +1,22 @@
+def minion_game(string):
+    # your code goes here
+    vowels = 'AEIOU'
+
+    kevin = 0
+    stuart = 0
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin += (len(string)-i)
+        else:
+            stuart += (len(string)-i)
+
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif kevin < stuart:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
+        
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
