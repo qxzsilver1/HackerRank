cfc35733a753f827bd62407b36e178e8bc3f1324
qxzsilver1 <hk442@cornell.edu>
2017-02-06 21:12:52 -0500

    Create solution.py

---
+++ Python/Sets/Check-Subset/Pypy2/solution.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+for i in range(int(raw_input())): #More than 4 lines will result in 0 score. Blank lines won't be counted.  
+    a = int(raw_input()); A = set(raw_input().split())
+    b = int(raw_input()); B = set(raw_input().split())
+    print A.intersection(B) == A
