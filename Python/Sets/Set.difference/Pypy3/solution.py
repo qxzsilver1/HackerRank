7ded84c935d53a6a019995a387b81f9cbaeb5a4e
qxzsilver1 <hk442@cornell.edu>
2017-02-06 20:46:59 -0500

    Create solution.py

---
+++ Python/Sets/Set.difference/Pypy3/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int, input().split())) 
+
+m = int(input())
+t = set(map(int, input().split()))
+
+print(len(s.difference(t)))
