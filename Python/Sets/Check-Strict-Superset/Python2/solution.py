d7738b55e407e33ada24ce939bd9f81f510847c6
qxzsilver1 <hk442@cornell.edu>
2017-02-06 21:23:08 -0500

    Create solution.py

---
+++ Python/Sets/Check-Strict-Superset/Python2/solution.py
@@ -0,0 +1,7 @@
+A = set(raw_input().split())
+is_strict_superset = True
+for i in range(int(raw_input())): 
+    B = set(raw_input().split())
+    is_strict_superset = is_strict_superset and (A.intersection(B) == B and len(A) > len(B))
+    
+print is_strict_superset
