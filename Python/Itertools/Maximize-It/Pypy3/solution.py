f2feb2d42c6607236ed4f4b305615ebccc58f72c
qxzsilver1 <hk442@cornell.edu>
2017-02-08 17:00:35 -0500

    Create solution.py

---
+++ Python/Itertools/Maximize-It/Pypy3/solution.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import product
+
+K, M = [int(x) for x in input().split()]
+arrayN = []
+for _i_ in range(K):
+    arrayN.append([int(x) for x in input().split()][1:])
+
+possible_combination = list(product(*arrayN))
+
+def func(nums):
+    return sum(x*x for x in nums) % M
+
+print(max(list(map(func, possible_combination))))
