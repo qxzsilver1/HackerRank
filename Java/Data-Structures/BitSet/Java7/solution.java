29519967ac5bed4f054124eef3e95966582dc589
qxzsilver1 <hk442@cornell.edu>
2017-02-08 00:33:20 -0500

    Create solution.java

---
+++ Java/Data-Structures/BitSet/Java7/solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        BitSet b1 = new BitSet(size);
+        BitSet b2 = new BitSet(size);
+        int op = sc.nextInt();
+        String s = null;
+        int i, j;
+        while (op > 0) {
+            s = sc.next();
+            i = sc.nextInt();
+            j = sc.nextInt();
+            switch(s) {
+                case("AND"): 
+                  if (i == 1) 
+                    b1.and(b2); 
+                  else 
+                    b2.and(b1);
+                  break;
+                case("OR"): 
+                  if (i == 1)
+                    b1.or(b2);
+                  else 
+                    b2.or(b1);
+                  break;
+                case("XOR"):
+                  if (i == 1)
+                    b1.xor(b2);
+                  else 
+                    b2.xor(b1);
+                  break;
+                case("FLIP"): 
+                  if (i == 1)
+                    b1.flip(j);
+                  else 
+                    b2.flip(j);
+                  break;
+                case("SET"): 
+                  if (i == 1)
+                    b1.set(j);
+                  else 
+                    b2.set(j);
+                  break;
+                default : ;
+            }
+            
+            System.out.println(b1.cardinality()+" "+b2.cardinality());
+            op--;
+        }
+    }
+}
