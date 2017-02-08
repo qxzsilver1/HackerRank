09b203933fbb8bf0d92698243599adda76a1a59b
qxzsilver1 <hk442@cornell.edu>
2017-02-08 17:24:28 -0500

    Create solution.py

---
+++ Python/Collections/collections.Counter/Python3/solution.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+n = int(input())
+shoes = Counter(map(int, input().split()))
+x = int(input())
+
+income = 0
+
+for i in range(x):
+    size, price = map(int, input().split())
+    if shoes[size]: 
+        income += price
+        shoes[size] -= 1
+
+print(income)
