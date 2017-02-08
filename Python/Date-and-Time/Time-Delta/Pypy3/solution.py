7f0123180e623dc7a72999d1fbc373e42eeeb974
qxzsilver1 <hk442@cornell.edu>
2017-02-08 18:32:53 -0500

    Create solution.py

---
+++ Python/Date-and-Time/Time-Delta/Pypy3/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from datetime import datetime as dt
+
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(), fmt) - 
+                   dt.strptime(input(), fmt)).total_seconds())))
