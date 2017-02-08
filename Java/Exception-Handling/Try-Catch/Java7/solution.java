9eb982a706ab2c80ee854e9d081b84a6b88098a0
qxzsilver1 <hk442@cornell.edu>
2017-02-08 05:41:21 -0500

    Create solution.java

---
+++ Java/Exception-Handling/Try-Catch/Java7/solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        try (Scanner scanner = new Scanner(System.in)) {
+	        System.out.println(scanner.nextInt()/scanner.nextInt());
+        } catch (ArithmeticException aex) {
+	        System.out.println(aex);
+        } catch (InputMismatchException imex) {
+	        System.out.println(imex.getClass().getName());
+        }
+    }
+}
