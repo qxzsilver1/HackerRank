9fbb6753198880050e8aaabde5ba1cc9dac4b857
qxzsilver1 <hk442@cornell.edu>
2017-02-08 17:37:00 -0500

    Create solution.py

---
+++ Python/Collections/Collections.namedtuple/Pypy3/solution.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import namedtuple
+n, Student = int(input()), namedtuple('Student', input())
+print("%.2f" %( sum([float(stud.MARKS) for stud in [Student(*input().split()) for _ in range(n)]]) / n ))
