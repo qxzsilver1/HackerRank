8e94fe99e5cae2f996dbc00c3f4c867e955ded13
qxzsilver1 <hk442@cornell.edu>
2017-02-07 19:04:49 -0500

    Create solution.rb

---
+++ Distributed-Systems/MapReduce-Basics/Tutorials-3-Composite-Keys/Ruby/solution.rb
@@ -0,0 +1,73 @@
+require 'json'
+
+class MapReduce
+    
+    def initialize
+        @intermediate = Hash.new{|x,y|x[y]=Array.new}
+        @finalResult = []
+    end
+     
+    def emitIntermediate(key, value)
+        @intermediate[key] += [value]
+    end
+
+    def emit(value)
+        @finalResult += [value]
+    end
+
+    def execute(data, mapper, reducer)
+        data.each do |line|
+       warn "Loading data:#{line}"
+            record = JSON.parse(line)
+            mapper(self,record)
+        end
+
+        @intermediate.keys.each do |key|
+            reducer(self,key,@intermediate[key])
+        end
+
+        
+        @finalResult.each do |item|
+            outputMap = Hash.new
+            outputMap["key"] = item.key
+            outputMap["value"] = item.value
+            puts outputMap.to_json
+        end
+    end
+end
+
+class Pair
+    attr_accessor :key, :value
+
+    def initialize(key,value)
+        @key = key
+        @value = value
+    end
+end
+
+def mapper(mapred,record)
+    country_and_state = record["key"]
+    population = record["value"]
+    # COMPLETE THE LINE BELOW AND FILL UP THE QUESTION MARKS
+    mapred.emitIntermediate(country_and_state,population)
+end
+
+def reducer(mapred,key,list_of_values)
+    # COMPLETE THE LINE BELOW AND FILL UP THE QUESTION MARKS
+    total = 0.0
+    list_of_values.each do |item|
+        total += item.to_f
+    end
+    total=total+0.5
+    population = total.to_i
+    resultPair = Pair.new(key,population)
+    mapred.emit(resultPair)
+end
+
+inputData = []
+while line = gets()
+    country, state, city, population = line.strip.split("\t")
+    inputData += [{"key"=>"#{country},#{state}","value"=>population}.to_json]
+end
+@mapred = MapReduce.new()
+@mapred.execute(inputData,method(:mapper),method(:reducer))
