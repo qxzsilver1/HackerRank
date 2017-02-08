4bf6f030c76080a5755b5b12cb68b1b24ea91a8a
qxzsilver1 <hk442@cornell.edu>
2017-02-08 07:24:01 -0500

    Create main.java

---
+++ Java/Advanced/Singleton-Pattern/Java7/main.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    private Singleton() {}
+    public String str;
+    private static final Singleton instance = new Singleton();
+    static Singleton getSingleInstance() { return instance; }
+}
