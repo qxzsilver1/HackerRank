ff5f217e23a7d4d32587f6c571701e439098117d
qxzsilver1 <hk442@cornell.edu>
2017-02-08 23:41:06 -0500

    Create solution.py

---
+++ Python/Closures-and-Decorators/Standardize-Mobile-Number-Using-Decorators/Python2/solution.py
@@ -0,0 +1,13 @@
+def wrapper(f):
+    def fun(l):
+        # complete the function
+        f("+91 "+c[-10:-5]+" "+c[-5:] for c in l)
+    return fun
+    
+@wrapper
+def sort_phone(l):
+    print '\n'.join(sorted(l))
+
+if __name__ == '__main__':
+    l = [raw_input() for _ in range(int(input()))]
+    sort_phone(l) 
