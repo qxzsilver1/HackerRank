cef4f393268ec57cae7dd111336b0ed2847c6f2a
qxzsilver1 <hk442@cornell.edu>
2017-02-08 21:40:27 -0500

    Update solution.py

--- Python/Functionals/Map-and-Lambda-Function/Python2/solution.py
+++ Python/Functionals/Map-and-Lambda-Function/Python2/solution.py
@@ -3,7 +3,7 @@ cube = lambda x: x**3 # complete the lambda function
 def fibonacci(n):
     # return a list of fibonacci numbers
     fib = lambda n: n if n < 2 else fib(n - 1) + fib(n - 2)
-    fibs = lambda n: map(fib, range(n))
+    fibs = lambda n: map(fib, xrange(n))
     return fibs(n)
     
 if __name__ == '__main__':
