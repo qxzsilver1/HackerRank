5a8bfa1b23233c9852aab4ee6776be902f55449f
qxzsilver1 <hk442@cornell.edu>
2017-02-08 20:38:10 -0500

    Create solution.py

---
+++ Python/Built-Ins/Sort-Data/Pypy3/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, M = map(int, input().split())
+rows = [input() for _ in range(N)]
+K = int(input())
+
+for row in sorted(rows, key=lambda row: int(row.split()[K])):
+    print(row)
