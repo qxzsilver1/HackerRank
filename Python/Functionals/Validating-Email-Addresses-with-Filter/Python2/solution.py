eb0807f7a9243153821fbd8850f0b7986220b9cb
qxzsilver1 <hk442@cornell.edu>
2017-02-08 21:47:28 -0500

    Create solution.py

---
+++ Python/Functionals/Validating-Email-Addresses-with-Filter/Python2/solution.py
@@ -0,0 +1,29 @@
+def fun(s):
+    # return True if s is a valid email, else return False
+    try:
+        username, url = s.split("@")
+        website, extension = url.split(".")
+    except ValueError:
+        return False
+    
+    if username.replace("-", "").replace("_", "").isalnum() is False:
+        return False
+    elif website.isalnum() is False:
+        return False
+    elif len(extension) > 3:
+        return False
+    else:
+        return True
+
+def filter_mail(emails):
+    return filter(fun, emails)
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    emails = []
+    for _ in range(n):
+        emails.append(raw_input())
+
+    filtered_emails = filter_mail(emails)
+    filtered_emails.sort()
+    print filtered_emails
