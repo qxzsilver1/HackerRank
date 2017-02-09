5b4c9d70b80ceb167a73019d2719bccdbe36c0e4
qxzsilver1 <hk442@cornell.edu>
2017-02-08 22:04:16 -0500

    Create solution.py

---
+++ Python/Functionals/Reduce-Function/Python2/solution.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from fractions import Fraction
+
+def product(fracs):
+    t = Fraction(reduce(lambda x,y: Fraction(x*y), fracs)) # complete this line with a reduce statement
+    return t.numerator, t.denominator
+    
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(input()):
+        fracs.append(Fraction(*map(int, raw_input().split())))
+    result = product(fracs)
+    print(*result)
