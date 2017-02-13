ee9ea9079153b6c726727b315c164bf08b603bf6
qxzsilver1 <hk442@cornell.edu>
2017-02-13 04:44:30 -0500

    Create solution.py

---
+++ Python/Numpy/Floor-Ceil-and-Rint/Pypy2/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+
+A = np.array((raw_input().split()), float)
+print (np.floor(A))
+print (np.ceil(A))
+print (np.rint(A))
