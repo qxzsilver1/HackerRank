728fb7bd2e641e834e2fbc3a3c6f2520f25ed56f
qxzsilver1 <hk442@cornell.edu>
2017-02-12 22:42:13 -0500

    Create solution.py

---
+++ Python/Numpy/Array-Mathematics/Pypy2/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+
+n, m = map(int, raw_input().split())
+a, b = (np.array([raw_input().split() for _ in xrange(n)], dtype=int) for _ in xrange(2))
+
+print a+b, '\n', a-b, '\n', a*b, '\n', a//b, '\n', a%b, '\n', a**b
