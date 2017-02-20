18459c9a4a947745601d876d22392b339f5c2577
qxzsilver1 <hk442@cornell.edu>
2017-02-19 19:36:15 -0500

    Create solution.py

---
+++ Python/Numpy/Sum-and-Prod/Pypy2/solution.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy
+
+n, m = map(int, raw_input().split())
+a = numpy.array([raw_input().split() for _ in xrange(n)], int)
+
+print (numpy.product(numpy.sum(a, axis = 0), axis = 0))
