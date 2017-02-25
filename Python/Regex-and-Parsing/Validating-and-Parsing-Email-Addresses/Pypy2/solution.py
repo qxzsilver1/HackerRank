819f2e74514ab3f54f6a3c89d6635e0785d57bc7
qxzsilver1 <hk442@cornell.edu>
2017-02-25 02:21:28 -0500

    Create solution.py

---
+++ Python/Regex-and-Parsing/Validating-and-Parsing-Email-Addresses/Pypy2/solution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+import email.utils as eu
+
+for _ in xrange(input()):
+    for i,a in [eu.parseaddr(raw_input())]:
+        if re.match(r'^[a-z][\w.-]*@[a-z]+\.[a-z]{1,3}$',a):
+            print eu.formataddr((i,a))
