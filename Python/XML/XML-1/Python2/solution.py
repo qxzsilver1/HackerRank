65d7343aaf80fcbda7ee8db476a3bd5317f0191f
qxzsilver1 <hk442@cornell.edu>
2017-02-26 22:03:03 -0500

    Create solution.py

---
+++ Python/XML/XML-1/Python2/solution.py
@@ -0,0 +1,16 @@
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    # your code goes here
+    count = len(node.attrib)
+    for x in node:
+        count += get_attr_number(x)
+    return count
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print get_attr_number(root)
