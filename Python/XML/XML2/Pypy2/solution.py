af930eef282ecdcd051bc528b47741c049f4bd3a
qxzsilver1 <hk442@cornell.edu>
2017-02-26 22:09:25 -0500

    Update solution.py

--- Python/XML/XML2/Pypy2/solution.py
+++ Python/XML/XML2/Pypy2/solution.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as etree
 maxdepth = 0
 def depth(elem, level):
     global maxdepth
-    
+    # your code goes here
     if (level == maxdepth):
         maxdepth += 1
         
