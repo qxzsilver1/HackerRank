842f69b6df80664141ee0dae6ca76590e91db32f
qxzsilver1 <hk442@cornell.edu>
2017-02-27 14:15:37 -0500

    Create solution.sql

---
+++ SQL/Aggregation/Weather-Observation-Station-16/DB2/solution.sql
@@ -0,0 +1,8 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+select decimal(round(min(LAT_N),4),9,4) from STATION where LAT_N > 38.7780;
