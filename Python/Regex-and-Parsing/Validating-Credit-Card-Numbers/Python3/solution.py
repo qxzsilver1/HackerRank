37df226aaca3fcbe2a45a6b790b19bc5a8e7ac13
qxzsilver1 <hk442@cornell.edu>
2017-02-26 21:43:08 -0500

    Create solution.py

---
+++ Python/Regex-and-Parsing/Validating-Credit-Card-Numbers/Python3/solution.py
@@ -0,0 +1,3 @@
+import re
+
+[print('Valid' if re.match(r'^[456]\d{3}(?:-?\d{4}){3}$', card) and not re.search(r'(\d)\1{3}', re.sub('-', '', card)) else 'Invalid') for card in [input() for _ in range(int(input()))]]
