e42817e8ac4994b5ac64644d9459da074220dd13
qxzsilver1 <hk442@cornell.edu>
2017-02-26 21:12:15 -0500

    Create solution.py

---
+++ Python/Regex-and-Parsing/HTML-Parser-Part-1/Python3/solution.py
@@ -0,0 +1,22 @@
+import re
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ("Start :", tag)
+        self.value(attrs)
+
+    def handle_endtag(self, tag):
+        print ("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print ("Empty :", tag)
+        self.value(attrs)
+
+    def value(self, attrs = None):
+        if attrs:
+            [print('->', attr, '>', val) for attr, val, in attrs]
+
+ss = '\n'.join([input() for x in range(int(input()))])
+parser = MyHTMLParser()
+parser.feed(ss)
