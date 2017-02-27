657901e8fe634e039c2eb857ea63b5b65562e0e9
qxzsilver1 <hk442@cornell.edu>
2017-02-27 12:55:20 -0500

    Create solution.sql

---
+++ SQL/Aggregation/Weather-Observation-Station-2/DB2/solution.sql
@@ -0,0 +1,8 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+select decimal(round(sum(LAT_N),2),9,2), decimal(round(sum(LONG_W),2),9,2) from STATION;
