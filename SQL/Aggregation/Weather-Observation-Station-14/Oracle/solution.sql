c1fd5b7799ccac8200057c3beadb7d11cdd65b23
qxzsilver1 <hk442@cornell.edu>
2017-02-27 13:46:55 -0500

    Create solution.sql

---
+++ SQL/Aggregation/Weather-Observation-Station-14/Oracle/solution.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+Select round(Max(LAT_N),4) from STATION where LAT_N < 137.2345;
