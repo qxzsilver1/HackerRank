ae60dd83256542b4e858eb4bdee4942750c359d9
qxzsilver1 <hk442@cornell.edu>
2017-02-26 21:33:43 -0500

    Create solution.py

---
+++ Python/Regex-and-Parsing/Detect-HTML-Tags-Attributes-and-Attribute-Values/Pypy3/solution.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+
+class parseHTML(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        self.value(attrs)
+        
+    def value(self, attrs = None):
+        if attrs:
+            [print('->', attr, '>', val) for attr, val, in attrs]
+
+ss = '\n'.join([input() for x in range(int(input()))])
+parser = parseHTML()
+parser.feed(ss)
