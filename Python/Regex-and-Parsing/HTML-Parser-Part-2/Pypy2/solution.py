423b3d68a911e13beee337ee1cdb3f2be5223e6e
qxzsilver1 <hk442@cornell.edu>
2017-02-26 21:27:18 -0500

    Create solution.py

---
+++ Python/Regex-and-Parsing/HTML-Parser-Part-2/Pypy2/solution.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' in data:
+            print '>>> Multi-line Comment'
+        else:
+            print '>>> Single-line Comment'
+            
+        print data
+    
+    def handle_data(self, data):
+        if data == '\n': return
+        print '>>> Data'
+        print data
+
+html = ""       
+for i in range(int(raw_input())):
+    html += raw_input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
