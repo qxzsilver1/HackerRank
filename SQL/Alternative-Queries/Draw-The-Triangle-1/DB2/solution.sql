4746e24391cd632ebc73427802b3cde3dd420a74
qxzsilver1 <hk442@cornell.edu>
2017-02-27 19:04:24 -0500

    Create solution.sql

---
+++ SQL/Alternative-Queries/Draw-The-Triangle-1/DB2/solution.sql
@@ -0,0 +1,14 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+with x(id,val) as 
+(
+ select 1, REPEAT('*',1) from sysibm.sysdummy1
+ union all
+ select id+1, REPEAT('*',id+1) from x where id < 20
+)
+select val from x;
