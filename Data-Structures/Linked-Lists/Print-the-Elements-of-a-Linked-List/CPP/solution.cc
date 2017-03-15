c0088f793f59e6b68094955d403102bd96299d91
qxzsilver1 <hk442@cornell.edu>
2017-03-15 17:05:08 -0400

    Create solution.cc

---
+++ Data-Structures/Linked-Lists/Print-the-Elements-of-a-Linked-List/CPP/solution.cc
@@ -0,0 +1,20 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    Node *curr = head;
+    while (curr != NULL) {
+        cout << curr->data << endl;
+        curr = curr->next;
+    }
+}
