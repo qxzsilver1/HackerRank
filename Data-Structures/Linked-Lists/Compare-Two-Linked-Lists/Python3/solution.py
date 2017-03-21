4cdb938b9add81a26376060439aa80fb5db99a56
qxzsilver1 <hk442@cornell.edu>
2017-03-20 21:30:49 -0400

    Create solution.py

---
+++ Data-Structures/Linked-Lists/Compare-Two-Linked-Lists/Python3/solution.py
@@ -0,0 +1,22 @@
+"""
+ Merge two linked list
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+
+def CompareLists(headA, headB):
+    if (headA == None and headB == None):
+        return 1
+    
+    if (headA != None and headB != None):
+        return int(headA.data == headB.data) and CompareLists(headA.next, headB.next)
+    
+    return 0
