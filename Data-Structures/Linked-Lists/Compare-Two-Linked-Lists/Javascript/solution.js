62a9d0068bccc202ffb0185a61650f4082b0e101
qxzsilver1 <hk442@cornell.edu>
2017-04-19 16:43:31 -0400

    Create solution.js

---
+++ Data-Structures/Linked-Lists/Compare-Two-Linked-Lists/Javascript/solution.js
@@ -0,0 +1,21 @@
+/*
+    Compare two Linked Lists A and B
+    Return 1 if they are identical and 0 if they are not.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function compareLinkedLists( headA, headB) {
+    if (headA == null && headB == null) return 1;
+    
+    if (headA != null && headB != null)
+        return (((headA.data == headB.data) | 0) && compareLinkedLists(headA.next, headB.next)) | 0
+        
+    return 0
+}
