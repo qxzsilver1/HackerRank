46a8a8a239d3752825308457e3b4f4dd7ebc4277
qxzsilver1 <hk442@cornell.edu>
2017-04-30 22:49:56 -0400

    Create solution.java

---
+++ Data-Structures/Trees/Level-Order-Traversal/Java7/solution.java
@@ -0,0 +1,25 @@
+   /* 
+    
+    class Node 
+       int data;
+       Node left;
+       Node right;
+   */
+
+   void LevelOrder(Node root)
+    {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+       
+        while (!q.isEmpty()) {
+            Node tempNode = q.poll();
+            System.out.print(tempNode.data + " ");
+            
+            if (tempNode.left != null)
+                q.add(tempNode.left);
+            
+            if (tempNode.right != null)
+                q.add(tempNode.right);
+        }
+      
+    }
