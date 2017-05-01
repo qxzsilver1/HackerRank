35421015e2f655ceaab3b98c75cd6e9754777471
qxzsilver1 <hk442@cornell.edu>
2017-04-30 23:19:29 -0400

    Create solution.java

---
+++ Data-Structures/Trees/Binary-Search-Tree-Insertion/Java7/solution.java
@@ -0,0 +1,28 @@
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node Insert(Node root,int value)
+    {
+        if (root == null) {
+            Node newRoot = new Node();
+            newRoot.data = value;
+            newRoot.left = null;
+            newRoot.right = null;
+            return newRoot;
+        }
+    
+        if (value < root.data)
+            root.left = Insert(root.left, value);
+        else if (value > root.data)
+            root.right = Insert(root.right, value);
+            
+        return root;
+       
+    }
+
+
