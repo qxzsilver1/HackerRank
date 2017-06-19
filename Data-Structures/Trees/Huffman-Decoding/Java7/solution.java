6e57a52e03b974cbb97f59e44636370a86131dd7
qxzsilver1 <hk442@cornell.edu>
2017-06-19 18:35:18 -0400

    Create solution.java

---
+++ Data-Structures/Trees/Huffman-Decoding/Java7/solution.java
@@ -0,0 +1,20 @@
+/*  
+   class Node
+      public  int frequency; // the frequency of this tree
+       public  char data;
+       public  Node left, right;
+    
+*/ 
+
+void decode(String S ,Node root)
+    {
+        Node temp = root;
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            temp = c == '0' ? temp.left : temp.right;
+            if (temp.data != '\0') {
+                System.out.print(temp.data);
+                temp = root;
+            }
+        }       
+    }
