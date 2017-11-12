1324f8d1b1a9337c0d9b7193d4fc3c7c02b406d0
qxzsilver1 <hk442@cornell.edu>
2017-11-12 08:50:25 -0500

    Create solution.scala

---
+++ Functional-Programming/Introduction/HelloWorldNTimes/Scala/solution.scala
@@ -0,0 +1,22 @@
+object Solution extends App {
+
+object Solution {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+        //  Print "Hello World" on a new line 'n' times.
+        //f(n)
+    }
+    
+}
+
+def f(n : Int) = {
+	    for(i <- 0 until n){
+	        println("Hello World");
+	    }
+}
+
+  var n = scala.io.StdIn.readInt
+  f(n)
+}
