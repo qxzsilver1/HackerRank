4308f6d0a572d96a6cea5a80f67f03f4b10f6012
qxzsilver1 <hk442@cornell.edu>
2018-02-16 20:09:45 -0500

    Create solution.groovy

---
+++ Algorithms/Warmup/Solve-Me-First/Groovy/solution.groovy
@@ -0,0 +1,9 @@
+def solveMeFirst(a,b) { 
+    a+b
+}
+
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+def a = Integer.parseInt(br.readLine())
+def b = Integer.parseInt(br.readLine())
+def res = solveMeFirst(a,b)
+println res
