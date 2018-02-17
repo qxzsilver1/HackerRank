6d72418074f4828e226578f48973a939468d95da
qxzsilver1 <hk442@cornell.edu>
2018-02-16 19:59:27 -0500

    Create solution.d

---
+++ Algorithms/Warmup/Solve-Me-First/D/solution.d
@@ -0,0 +1,13 @@
+import std.stdio;
+int solveMeFirst (int a,int b) {
+    // Hint: Type return a+b below 
+     return a+b;
+}
+ 
+void main()
+{
+    int a,b,res;
+    readf("%d %d", &a,&b);
+    res = solveMeFirst(a,b);
+    writeln(res);
+}
