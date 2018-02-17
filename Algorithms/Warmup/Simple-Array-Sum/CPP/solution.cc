4e007e0cb0d2410583a56b9dc92641443de0d2ef
qxzsilver1 <hk442@cornell.edu>
2018-02-16 23:05:54 -0500

    Create solution.cc

---
+++ Algorithms/Warmup/Simple-Array-Sum/CPP/solution.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int simpleArraySum(int n, vector <int> ar) {
+    // Complete this function
+    int result = 0;
+    
+    for (int i = 0; i < n; i++)
+        result += ar[i];
+    
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = simpleArraySum(n, ar);
+    cout << result << endl;
+    return 0;
+}
