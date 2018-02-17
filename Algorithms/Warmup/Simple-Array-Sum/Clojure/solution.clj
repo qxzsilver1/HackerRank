e1b7bd06385d0ad2736af0e1a2adbcc5f2085b35
qxzsilver1 <hk442@cornell.edu>
2018-02-17 04:01:02 -0500

    Create solution.clj

---
+++ Algorithms/Warmup/Simple-Array-Sum/Clojure/solution.clj
@@ -0,0 +1,18 @@
+(use '[clojure.string :only (split triml)])
+
+(
+        let [
+          n_t (read-line) 
+          n (Integer/parseInt n_t) 
+        ]
+
+      (
+        let [
+          ar_temp (read-line) 
+          ar_t (split ar_temp #"\s+") 
+          ar (map #(Integer/parseInt %) ar_t) 
+        ]
+        
+        (println (reduce + ar))
+      )
+)
