84b6b1b01a44f5859bf345c89a3f13ed052663d7
qxzsilver1 <hk442@cornell.edu>
2018-02-18 05:45:58 -0500

    Create solution.cs

---
+++ Algorithms/Warmup/Compare-the-Triplets/CSharp/solution.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+        int[] scores = new int[2];
+    
+        scores[0] = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0);
+        scores[1] = ((a0<b0)?1:0)+ ((a1<b1)?1:0)+ ((a2<b2)?1:0);
+
+        return scores;
+    }
+
+    static void Main(String[] args) {
+        string[] tokens_a0 = Console.ReadLine().Split(' ');
+        int a0 = Convert.ToInt32(tokens_a0[0]);
+        int a1 = Convert.ToInt32(tokens_a0[1]);
+        int a2 = Convert.ToInt32(tokens_a0[2]);
+        string[] tokens_b0 = Console.ReadLine().Split(' ');
+        int b0 = Convert.ToInt32(tokens_b0[0]);
+        int b1 = Convert.ToInt32(tokens_b0[1]);
+        int b2 = Convert.ToInt32(tokens_b0[2]);
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        Console.WriteLine(String.Join(" ", result));
+        
+
+    }
+}
