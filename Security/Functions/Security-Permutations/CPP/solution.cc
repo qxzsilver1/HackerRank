0adc6f37dd06fc500f55b9c6170c7c6f928b5a68
qxzsilver1 <hk442@cornell.edu>
2018-02-20 22:01:03 -0500

    Create solution.cc

---
+++ Security/Functions/Security-Permutations/CPP/solution.cc
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    
+    int fn[n+1];
+    
+    fn[0] = 0;
+    
+    for(int i = 1; i <= n; i++) {
+        cin >> fn[i];
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        cout << fn[fn[i]] << endl;
+    }
+    return 0;
+}
