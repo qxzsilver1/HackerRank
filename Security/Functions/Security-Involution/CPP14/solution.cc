c7c3fe0e0e648e4b9b9a2cb79a9a56e0fbd164e0
qxzsilver1 <hk442@cornell.edu>
2018-04-03 00:54:12 -0400

    Create solution.cc

---
+++ Security/Functions/Security-Involution/CPP14/solution.cc
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    
+    int fn[n+1];
+    
+    fn[0] = 0;
+    
+    for(int i = 1; i <= n; i++) {
+        cin >> fn[i];
+    }
+    
+    bool involution = true;
+    
+    for(int i = 1; i <= n; i++) {
+        if (fn[fn[i]] != i) {
+            involution = false;
+            break;
+        }
+    }
+    
+    cout << (involution ? "YES" : "NO") << endl;
+    
+    return 0;
+}
