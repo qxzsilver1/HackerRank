8e5556fda9c0e11710d0fda054c746f64f10d61d
qxzsilver1 <hk442@cornell.edu>
2018-11-21 16:46:13 -0500

    Create solution.php

---
+++ 30-Days-of-Code/Day-7-Arrays/PHP/solution.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+fscanf($stdin, "%[^\n]", $arr_temp);
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+fclose($stdin);
+
+print join(' ', array_reverse($arr))
+
+?>
