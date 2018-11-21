97072b27c4bab228058d8e2e398949c670fd9102
qxzsilver1 <hk442@cornell.edu>
2018-11-21 17:10:47 -0500

    Create solution.py

---
+++ 30-Days-of-Code/Day-7-Arrays/Pypy2/solution.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    arr = map(int, raw_input().rstrip().split())
+
+    arr.reverse()
+
+    print ' '.join(map(str, arr))
