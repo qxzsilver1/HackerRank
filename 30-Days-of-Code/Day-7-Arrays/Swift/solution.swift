16d3a36fc608e168ec906fdf1a75b8703972f019
qxzsilver1 <hk442@cornell.edu>
2018-11-21 18:11:57 -0500

    Create solution.swift

---
+++ 30-Days-of-Code/Day-7-Arrays/Swift/solution.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine() else { fatalError("Bad input") }
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+var arrSomething = arr
+
+arrSomething.reverse()
+
+let something = arrSomething.map(String.init).joined(separator: " ")
+print(something)
