69fcf29bdddf7bca08d99383be750c00907d324c
qxzsilver1 <hk442@cornell.edu>
2018-12-21 03:34:40 -0500

    Create solution.cc

---
+++ Mathematics/Fundamentals/Leonardos-Prime-Factors/CPP/solution.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unsigned long long int gcd(unsigned long long int a, unsigned long long int b) {
+
+  while (b) {
+    unsigned long long int t = b;
+
+    b = a % b;
+    a = t;
+  }
+  return a;
+}
+
+/*
+ * Complete the primeCount function below.
+ */
+int primeCount(long n) {
+    /*
+     * Write your code here.
+     */
+    unsigned int count;
+    unsigned long long int prod;
+    unsigned long long int prime;
+
+    if (n < 2)
+      return 0;
+
+    prod = 2;
+    count = 1;
+    
+    for (prime = 3; prod * prime <= n; prime += 2) {
+      if (gcd(prod, prime) == 1) {
+        prod *= prime;
+        count++;
+      }
+    }
+
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = primeCount(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
