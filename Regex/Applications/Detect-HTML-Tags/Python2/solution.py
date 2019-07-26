f62ae0343e483822b55349d8ee4083cb297a526d
qxzsilver1 <hk442@cornell.edu>
2019-07-25 20:10:11 -0400

    Create solution.py

---
+++ Regex/Applications/Detect-HTML-Tags/Python2/solution.py
@@ -0,0 +1,26 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+Regex_Pattern = r"<[^>]*>"
+
+
+import re
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    tag_dict = {}
+    string_stripper = lambda x: x.strip("<>/")
+    string_splitter = lambda y: y.split(" ")[0]
+    matches = set()
+
+    for i in range(n):
+        s = raw_input().strip()
+        #print(s)
+        match = re.findall(Regex_Pattern, s)
+
+        match = list(map(string_stripper, match))
+        match = list(map(string_splitter, match))
+        matches = set(match).union(matches)
+
+        #print(match)
+
+    print(';'.join(map(str,list(sorted(matches)))))
