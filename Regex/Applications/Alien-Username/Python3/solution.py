ccc610cdc0957845a81235800974b91f9582da98
qxzsilver1 <hk442@cornell.edu>
2019-10-16 21:52:37 -0400

    Create solution.py

---
+++ Regex/Applications/Alien-Username/Python3/solution.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+Regex_Pattern = r'^(_|\.)[0-9]+[a-zA-Z]*_?$'    # Do not delete 'r'.
+import re
+
+n = int(input())
+# text = "\n".join(input() for _ in range(n))
+
+for _ in range(n):
+    if re.match(Regex_Pattern, input()) is None:
+        print('INVALID')
+    else:
+        print('VALID')
