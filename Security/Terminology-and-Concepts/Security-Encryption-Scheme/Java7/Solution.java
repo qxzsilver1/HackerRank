bb0fe56a6346f4f54a71f48524e6172598ebc7a6
qxzsilver1 <hk442@cornell.edu>
2019-10-16 22:03:23 -0400

    Create Solution.java

---
+++ Security/Terminology-and-Concepts/Security-Encryption-Scheme/Java7/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner (System.in);
+
+        int n = sc.nextInt();
+        sc.close();
+        
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans*=i;
+        } 
+
+        System.out.println(ans);
+    }
+}
+
