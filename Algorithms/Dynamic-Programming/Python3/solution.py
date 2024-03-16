5e3c1cf9bd4055a4d583500221ae7cc4daf93d25
qxzsilver1 <hk442@cornell.edu>
2024-03-16 14:49:39 -0400

    Create solution.py

---
+++ Algorithms/Dynamic-Programming/Python3/solution.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+def stockmax(prices):
+    # Write your code here
+    profit = 0
+    curr_max = prices[-1]
+    
+    for i in range(len(prices)-1, -1, -1):
+        if curr_max <= prices[i]:
+            curr_max = prices[i]
+            
+        profit += curr_max - prices[i]
+    
+    return profit
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
