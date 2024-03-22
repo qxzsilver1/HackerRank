a304b7e80fe5a13795f0214a859918b86020e519
qxzsilver1 <hk442@cornell.edu>
2024-03-22 10:27:08 -0400

    Create solution.py

---
+++ Algorithms/Search/Ice-Cream-Parlor/Python3/solution.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    res = []
+    lookup = dict()
+    
+    for i in range(len(arr)):
+        complement = m - arr[i]
+        
+        if complement in lookup:
+            return [lookup[complement] + 1, i + 1] if i > lookup[complement] else [i + 1, lookup[complement] + 1]
+        else:
+            lookup[arr[i]] = i
+    
+    return None
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
