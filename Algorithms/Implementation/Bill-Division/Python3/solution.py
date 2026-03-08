5bd87427f2c7cfa086364e19115467bc10aff6b2
qxzsilver1 <hk442@cornell.edu>
2026-03-08 12:36:25 -0400

    Create solution.py

---
+++ Algorithms/Implementation/Bill-Division/Python3/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    anna_portion = (sum(bill) - bill[k]) / 2
+    if float(b) == anna_portion:
+        print('Bon Appetit')
+    else:
+        print(int(b - anna_portion))
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
