d30e33fc4a0554877b7d657fd1b37d7a0060e314
qxzsilver1 <hk442@cornell.edu>
2026-03-07 23:59:07 -0500

    Create solution.py

---
+++ Algorithms/Warmup/Birthday-Cake-Candles/Python3/solution.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    ctr = 0
+    max_val = 0
+    
+    for c in candles:
+        if c > max_val:
+            max_val = c
+            ctr = 1
+        elif c == max_val:
+            ctr += 1
+        else:
+            continue
+    
+    return ctr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
