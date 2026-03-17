ae48d73c34b1c2af9d12f12f8f4b9e60682292f9
qxzsilver1 <hk442@cornell.edu>
2026-03-17 11:41:47 -0400

    Create solution.py

---
+++ Data-Structures/Arrays/Sparse-Arrays/Python3/solution.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+from collections import Counter
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    # Write your code here
+    ref_dict = Counter(stringList)
+    
+    res_list = [0] * len(queries)
+    
+    for i in range(len(queries)):
+        if queries[i] not in ref_dict:
+            continue
+            
+        res_list[i] = ref_dict[queries[i]]
+    
+    return res_list
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
