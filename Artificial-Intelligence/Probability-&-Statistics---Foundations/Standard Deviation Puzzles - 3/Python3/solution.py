36bb887c1db4155e318157260c7c6a53e21906db
qxzsilver1 <hk442@cornell.edu>
2026-04-29 17:15:36 -0400

    Create solution.py

---
+++ Artificial-Intelligence/Probability-&-Statistics---Foundations/Standard Deviation Puzzles - 3/Python3/solution.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import sqrt
+
+X_std = 3
+Y_std = 4
+
+X_Y_std = sqrt(X_std ** 2 + Y_std ** 2)
+
+print(format(X_Y_std, '.1f'))
