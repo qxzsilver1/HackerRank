6d45799e73c8be51c5eff9f792ba8b6b102e988c
qxzsilver1 <hk442@cornell.edu>
2026-05-28 10:33:02 -0400

    Create solution.py

---
+++ Artificial-Intelligence/Statistics-and-Machine-Learning/Correlation and Regression Lines - A Quick Recap #4/Python3/solution.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+y = 7
+
+# correct - this is the line of x on y
+x = (107 + 9 * 7) / 20
+
+# wrong - this is for line of y on x
+# x = (-33 + 5 * 7) / 4
+
+print('{0:.1f}'.format(x))
