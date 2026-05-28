cfb5b1c62bfc04f2d21db72df9da8b6b9745d911
qxzsilver1 <hk442@cornell.edu>
2026-05-28 10:46:08 -0400

    Create solution.py

---
+++ Artificial-Intelligence/Statistics-and-Machine-Learning/Correlation and Regression Lines - A Quick Recap #5/Python3/solution.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+m1 = 4/5
+m2 = 9/20
+
+s_x = 3
+var_x = 3 ** 2
+
+var_y = m1 / m2 * var_x
+
+
+print('{0:.1f}'.format(var_y))
